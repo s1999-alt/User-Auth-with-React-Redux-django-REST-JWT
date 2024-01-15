019d5156e417094aba623c777ffa78d240073845
Siyad Savad <siyadsavad313@gmail.com>
2024-01-15 13:38:00 +0530

    initial commit

---
+++ New folder/React_Redux_Jwt/React_Redux_Jwt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for React_Redux_Jwt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'React_Redux_Jwt.settings')
+
+application = get_asgi_application()
